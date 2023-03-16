73218bb35a438422b80c0776603351de90b77694
Muhammad Novan Saputra <93202567+Novansr@users.noreply.github.com>
2023-03-16 20:50:04 +0800

    Add files via upload

---
+++ Posttest1_P.Visual/Posttest1_P.Visual/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(66, 237)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(227, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Satuan Panjang"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(77, 304)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(206, 36)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Hasil Konversi"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(225, 375)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(166, 51)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Konversi"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(350, 234)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 38)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(482, 234)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 38)
+        Me.TextBox2.TabIndex = 4
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Kilometer", "Hektometer", "Dekameter", "Meter", "Desimeter", "Centimeter", "Milimeter"})
+        Me.ComboBox1.Location = New System.Drawing.Point(629, 234)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 39)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Kilometer", "Hektometer", "Dekameter", "Meter", "Desimeter", "Centimeter", "Milimeter"})
+        Me.ComboBox2.Location = New System.Drawing.Point(629, 304)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 39)
+        Me.ComboBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(350, 304)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 38)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(482, 304)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 38)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(482, 375)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(149, 44)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(256, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(562, 46)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Mengkonversi Satuan Panjang"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(367, 181)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 29)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Nilai"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(495, 181)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 29)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Satuan"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ClientSize = New System.Drawing.Size(1107, 465)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Posttest1_P.Visual/Posttest1_P.Visual/Form1.vb
@@ -0,0 +1,157 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Mendefinisikan variabel dan menghitung konversi'
+        Dim input As Decimal = TextBox1.Text
+        Dim output As Decimal
+        Dim fromUnit As String = ComboBox1.SelectedItem
+        Dim toUnit As String = ComboBox2.SelectedItem
+
+        Select Case fromUnit
+            Case "Kilometer"
+                Select Case toUnit
+                    Case "Kilometer"
+                        output = input
+                    Case "Hektometer"
+                        output = input * 10
+                    Case "Dekameter"
+                        output = input * 100
+                    Case "Meter"
+                        output = input * 1000
+                    Case "Desimeter"
+                        output = input * 10000
+                    Case "Centimeter"
+                        output = input * 100000
+                    Case "Milimeter"
+                        output = input * 1000000
+                End Select
+
+            Case "Hektometer"
+                Select Case toUnit
+                    Case "Kilometer"
+                        output = input / 10
+                    Case "Hektometer"
+                        output = input
+                    Case "Dekameter"
+                        output = input * 10
+                    Case "Meter"
+                        output = input * 100
+                    Case "Desimeter"
+                        output = input * 1000
+                    Case "Centimeter"
+                        output = input * 10000
+                    Case "Milimeter"
+                        output = input * 100000
+                End Select
+
+            Case "Dekameter"
+                Select Case toUnit
+                    Case "Kilometer"
+                        output = input / 100
+                    Case "Hektometer"
+                        output = input / 10
+                    Case "Dekameter"
+                        output = input
+                    Case "Meter"
+                        output = input * 10
+                    Case "Desimeter"
+                        output = input * 100
+                    Case "Centimeter"
+                        output = input * 1000
+                    Case "Milimeter"
+                        output = input * 10000
+                End Select
+
+            Case "Meter"
+                Select Case toUnit
+                    Case "Kilometer"
+                        output = input / 1000
+                    Case "Hektometer"
+                        output = input / 100
+                    Case "Dekameter"
+                        output = input / 10
+                    Case "Meter"
+                        output = input
+                    Case "Desimeter"
+                        output = input * 10
+                    Case "Centimeter"
+                        output = input * 100
+                    Case "Milimeter"
+                        output = input * 1000
+                End Select
+
+            Case "Desimeter"
+                Select Case toUnit
+                    Case "Kilometer"
+                        output = input / 10000
+                    Case "Hektometer"
+                        output = input / 1000
+                    Case "Dekameter"
+                        output = input / 100
+                    Case "Meter"
+                        output = input / 10
+                    Case "Desimeter"
+                        output = input
+                    Case "Centimeter"
+                        output = input * 10
+                    Case "Milimeter"
+                        output = input * 100
+                End Select
+
+            Case "Centimeter"
+                Select Case toUnit
+                    Case "Kilometer"
+                        output = input / 100000
+                    Case "Hektometer"
+                        output = input / 10000
+                    Case "Dekameter"
+                        output = input / 1000
+                    Case "Meter"
+                        output = input / 100
+                    Case "Desimeter"
+                        output = input / 10
+                    Case "Centimeter"
+                        output = input
+                    Case "Milimeter"
+                        output = input * 10
+                End Select
+
+            Case "Milimeter"
+                Select Case toUnit
+                    Case "Kilometer"
+                        output = input / 1000000
+                    Case "Hektometer"
+                        output = input / 100000
+                    Case "Dekameter"
+                        output = input / 10000
+                    Case "Meter"
+                        output = input / 1000
+                    Case "Desimeter"
+                        output = input / 100
+                    Case "Centimeter"
+                        output = input / 10
+                    Case "Milimeter"
+                        output = input
+                End Select
+        End Select
+
+        'Menampilkan hasil konversi 
+        TextBox3.Text = output.ToString()
+
+        'Menampilkan satuan dari input 
+        TextBox2.Text = fromUnit
+
+        'Menampilkan satuan dari output 
+        TextBox4.Text = toUnit
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Merestart program dengan mengembalikan nilai input dan output ke 0'
+        TextBox1.Text = ""
+        TextBox3.Text = ""
+        TextBox2.Clear()
+        TextBox4.Clear()
+        ComboBox1.SelectedIndex = -1
+        ComboBox2.SelectedIndex = -1
+    End Sub
+End Class
